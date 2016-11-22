0b14ef24b7766bb4f09b6e7a13ce429c037976ea
Nico Cucciniello <ncucciniello@gmail.com>
2016-11-22 13:44:19 -0500

    add App.jsx html structure

--- models/playlist.js
+++ models/playlist.js
@@ -15,31 +15,31 @@ function getPlaylist(req, res, next) {
   .catch(err => next(err));
 }
 
-save to playlist (post)
-function saveOneSongToPlaylist(req, res, next) {
-  db.none(`INSERT INTO playlist (trackid, trackname, artistname, collectionid, user_id)
-           VALUES ($1, $2, $3, $4, $5)`,
-          [/* either req.body or req.params */])
-  .then(results => {
-    // returns nothing
-    res.results = results;
-    next();
-  })
-  .catch(err => next(err));
-}
+// // save to playlist (post)
+// function saveOneSongToPlaylist(req, res, next) {
+//   db.none(`INSERT INTO playlist (trackid, trackname, artistname, collectionid, user_id)
+//            VALUES ($1, $2, $3, $4, $5)`,
+//           [/* either req.body or req.params */])
+//   .then(results => {
+//     // returns nothing
+//     res.results = results;
+//     next();
+//   })
+//   .catch(err => next(err));
+// }
 
-delete a song in the playlist(delete)
-function deleteOneSongFromPlaylist(req, res, next) {
-  db.result(`DELETE FROM playlist
-             WHERE user_id = $1 AND trackid = $2`,
-             [/* req.body or req.params */] )
-  .then(results => {
-    // returns null
-    res.results = results;
-    next();
-  })
-  .catch(err => next(err);
-}
+// // delete a song in the playlist(delete)
+// function deleteOneSongFromPlaylist(req, res, next) {
+//   db.result(`DELETE FROM playlist
+//              WHERE user_id = $1 AND trackid = $2`,
+//              [/* req.body or req.params */] )
+//   .then(results => {
+//     // returns null
+//     res.results = results;
+//     next();
+//   })
+//   .catch(err => next(err);
+// }
 
 module.exports = {
   getPlaylist,
