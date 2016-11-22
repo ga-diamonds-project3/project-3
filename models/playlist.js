5cfd820a565226b089ab04a88aac74bb6b9bf425
Nico Cucciniello <ncucciniello@gmail.com>
2016-11-22 11:45:01 -0500

    playlist API route working

--- models/playlist.js
+++ models/playlist.js
@@ -3,10 +3,11 @@ const db = require('../db/db');
 // get all playlist (get)
 function getPlaylist(req, res, next) {
   console.log('getting playlist ** server');
-  db.any(`SELECT * 
+  db.any(`SELECT *
           FROM playlist
-          WHERE user_id = $1;`, 
-          [/* req.params */])
+          WHERE user_id = $1;`,
+          [req.params.userid]
+          )
   .then(results => {
     res.results = results;
     next();
@@ -14,10 +15,10 @@ function getPlaylist(req, res, next) {
   .catch(err => next(err));
 }
 
-// save to playlist (post)
+save to playlist (post)
 function saveOneSongToPlaylist(req, res, next) {
   db.none(`INSERT INTO playlist (trackid, trackname, artistname, collectionid, user_id)
-           VALUES ($1, $2, $3, $4, $5)`, 
+           VALUES ($1, $2, $3, $4, $5)`,
           [/* either req.body or req.params */])
   .then(results => {
     // returns nothing
@@ -27,7 +28,7 @@ function saveOneSongToPlaylist(req, res, next) {
   .catch(err => next(err));
 }
 
-// delete a song in the playlist(delete)
+delete a song in the playlist(delete)
 function deleteOneSongFromPlaylist(req, res, next) {
   db.result(`DELETE FROM playlist
              WHERE user_id = $1 AND trackid = $2`,
@@ -39,3 +40,9 @@ function deleteOneSongFromPlaylist(req, res, next) {
   })
   .catch(err => next(err);
 }
+
+module.exports = {
+  getPlaylist,
+  // saveOneSongToPlaylist,
+  // deleteOneSongFromPlaylist,
+};
