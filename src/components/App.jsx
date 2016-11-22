43778209614850b3e02e1591a20285de9bac4846
Nico Cucciniello <ncucciniello@gmail.com>
2016-11-22 15:00:52 -0500

    create PlayList component and render on page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,54 @@
 // import the libs we need
 import React, { Component } from 'react';
+import PlayList from './PlayList/PlayList.jsx';
 import './normalize.css';
 import style from './App.css';
 
-handleYoutubeFetch () {
-  fetch(`http://localhost:3000/api/youtube`)
-  .then(r => r.json())
-  .then((video) => {
-    // Data pulled from Api, will be determined at a later time.
-  })
-  .catch(error) => console.log('You\'re looking at an Error: ', error)
-}
+// handleYoutubeFetch () {
+//   fetch(`http://localhost:3000/api/youtube`)
+//   .then(r => r.json())
+//   .then((video) => {
+//     // Data pulled from Api, will be determined at a later time.
+//   })
+//   .catch(error) => console.log('You\'re looking at an Error: ', error)
+// }
 
 // create a React Component called _App_
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      playlist: [],
+    };
+  }
+
+  // function that will hit our database API and set an array of data to the playlist state
+  getPlayList() {
+    fetch('/playlist/1')
+    .then(r => r.json())
+    .then((songs) => {
+      this.setState({
+
+        playlist: songs
+      });
+    })
+    .catch(err => console.log(err));
+  }
+
+  // handleDelete(trackid) {
+  //   fetch(`/api/puppies/${id}`, {
+  //     method: 'DELETE'
+  //   })
+  //   .then(() => {
+  //     const playlist = this.state.playlist.filter((track) => {
+  //       return track.trackid !== trackid;
+  //     })
+  //     this.setState({ playlist: playlist })
+  //   })
+  //   .catch(err => console.log(err));
+  // }
+
   render(){
     return (
       <div id="app-container">
@@ -23,9 +58,9 @@ class App extends Component {
         </header>
 
         <main>
-          <div class="titles">
+          <div className="titles">
             <h2>Albums</h2>
-            <h2 class="center-title">Songs</h2>
+            <h2 className="center-title">Songs</h2>
             <h2>PlayList</h2>
           </div>
 
@@ -34,14 +69,18 @@ class App extends Component {
 
             {/* SONG LIST COMPONENT GOES HERE (<SongList />)*/}
 
-            {/* PLAYLIST COMPONENT GOES HERE (<PlayList />)*/}
+            <PlayList
+              getPlayList={this.getPlayList.bind(this)}
+              playlist={this.state.playlist}
+              // handleDelete={this.handleDelete.bind(this)}
+             />
           </section>
         </main>
 
         <footer>
 
         </footer>
-      <div>
+      </div>
     );
   }
 }
---
+++ src/components/PlayList/PlayList.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PlayListItem from '../PlayListItem/PlayListItem.jsx';
+import style from './PlayList.css';
+
+class PlayList extends Component {
+
+  componentWillMount() {
+    this.props.getPlayList();
+  }
+
+
+  // This function will create a PlayListItem for each song saved to the playlist database
+  renderPlayList() {
+    return this.props.playlist.map((song, i) =>
+      <PlayListItem
+        // handleLikePuppy={this.props.handleLikePuppy}
+        // handleAbandonment={this.props.handleAbandonment}
+        name={song.trackname}
+        album={song.collectionname}
+        artist={song.artistname}
+        key={i}
+      />
+    );
+  }
+
+  render(){
+    console.log(this.props);
+    return (
+      <article className="fav-container">
+        {this.renderPlayList()}
+      </article>
+    );
+  }
+}
+
+export default PlayList;
