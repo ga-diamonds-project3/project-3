7d010d590befef608c473eb4788403da0ea22459
Sang Min Na <xsangmin@gmail.com>
2016-11-22 09:46:31 -0500

    created schema for playlist and programmed playlist.js for model

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+DROP TABLE IF EXISTS playlist;
+DROP TABLE IF EXISTS users;
+
+-- work on it more later
+CREATE TABLE users(
+  user_id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL,
+);
+
+CREATE TABLE playlist(
+  trackid INT NOT NULL,
+  trackname TEXT NOT NULL,
+  artistname TEXT NOT NULL,
+  collectionid INT NOT NULL, 
+  user_id INT NOT NULL,
+);
+
+COMMIT;
