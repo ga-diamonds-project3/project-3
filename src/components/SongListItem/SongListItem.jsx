4074955dfa641f783985775e4502a05cf0e94c02
Nico Cucciniello <ncucciniello@gmail.com>
2016-11-27 00:23:39 -0500

    create modal to house video on play click

--- src/components/SongListItem/SongListItem.jsx
+++ src/components/SongListItem/SongListItem.jsx
@@ -7,11 +7,14 @@ class SongListItem extends Component {
   render(){
     return (
       <div className="songItem">
-        <a target="_blank" href={this.props.preview}>
-          <div className="getVideo"> &#9658;
+        {/*<a target="_blank" href={this.props.preview}>*/}
+          <div
+            className="getVideo"
+            onClick={()=>{document.querySelector('.modal').style.display = "block"}}
+          > &#9658;
             {/*onClick={() => this.props.handleAbandonment(this.props.id)}*/}
           </div>
-        </a>
+        {/*</a>*/}
         <div className="addSong"
           onClick={this.props.changeSongSelected}
         >+
@@ -21,7 +24,14 @@ class SongListItem extends Component {
         {/*<p className="songAlbum">Album: {this.props.album}</p>*/}
         {/*<p className="songArtist">Artist: {this.props.artist}</p>*/}
 
-
+        <div className="modal">
+          <div className="modal-content">
+          <span
+            className="close"
+            onClick={()=>{document.querySelector('.modal').style.display = "none"}}
+          >×</span>
+          </div>
+        </div>
       </div>
     );
   }
