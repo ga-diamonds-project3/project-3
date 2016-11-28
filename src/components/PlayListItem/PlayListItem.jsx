7bff3df64a62965a17a19aec107d9136ea8b3262
Donald Highe <dhighe25@gmail.com>
2016-11-28 14:37:53 -0500

    omg it works 2

--- src/components/PlayListItem/PlayListItem.jsx
+++ src/components/PlayListItem/PlayListItem.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import MusicVideo from '../MusicVideo/MusicVideo.jsx';
 import style from './PlayListItem.css';
 
 // create a React Component called _App_
@@ -11,16 +10,7 @@ class PlayListItem extends Component {
         <a target="_blank" href={this.props.preview}>
           <div
             className="getVideo"
-            onClick={
-              ()=>{
-                console.log(this.props.name),
-                console.log(this.props.artist),
-                console.log(this.props.getMusicVideo(this.props.artist, this.props.name)),
-                // setTimeout(this.props.getMusicVideo(this.props.artist, this.props.name),0),
-                // this.props.getMusicVideo(),
-                document.querySelector('.modal').style.display = "block"
-              }
-            }
+            onClick={()=>{document.querySelector('.modal').style.display = "block"}}
           >&#9658;
           </div>
         </a>
@@ -34,26 +24,6 @@ class PlayListItem extends Component {
         <p className="favTrack">{this.props.name}</p>
         <p className="favAlbum">{this.props.album}</p>
         <p className="favArtist">by: {this.props.artist}</p>
-
-        <div className="modal">
-          <div className="modal-content">
-            <span
-            className="close"
-            onClick={()=>{document.querySelector('.modal').style.display = "none"}}
-            >×</span>
-            {/*<h4 className="placeholder-text">YOUTUBE VIDEO GOES HERE</h4>*/}
-            {/*<button
-            onClick={this.props.getMusicVideo.bind(this)}
-            >Play Music</button>*/}
-            <div
-            className="video"
-            >
-            <MusicVideo
-              videoId={this.props.videoId}
-            />
-            </div>
-          </div>
-        </div>
 
       </div>
     );
