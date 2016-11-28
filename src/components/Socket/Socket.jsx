cddfe1cc520e658cf3340f3efd5694be8915635f
Sang Min Na <xsangmin@gmail.com>
2016-11-27 23:34:00 -0500

    handleinputhchanges

--- src/components/Socket/Socket.jsx
+++ src/components/Socket/Socket.jsx
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 import './Socket.css';
 
 export default class Socket extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      msg : '',
+    }
+  }
+
   componentWillMount() {
     this.props.socket();
+  }
+
+  handleSubmit(e) {
+    console.log('submit event', e.target);
+    console.log('submit event', e.target.firstChild);
+    e.preventDefault();
+  }
+
+  handleInputChange(e) {
+    // console.log('input value:', e);
+    console.log(e.target.value)
+    this.setState({
+      msg: e.target.value,
+    });
   }
 
   render() {
     return(
       <div>
         <ul id="messages"></ul>
-        
-        <form type="submit">
-          <input id="m"/>
-          <button onClick={event => this.handleSubmit(event)}>Send</button>
+
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input id="m" onChange={event => this.handleInputChange(event)}/>
+          <button>Send</button>
         </form>
       </div>
     )
